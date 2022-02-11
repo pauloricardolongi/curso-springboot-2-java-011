06f32fc5a93f5c5f1ca4dadcde2519d17a06687e
pauloricardolongi <pauloricardolongi@gmail.com>
2022-02-11 04:59:53 -0300

    Classe Caterogy

--- src/main/java/com/educandoweb/curso/resources/CategoryResource.java
+++ src/main/java/com/educandoweb/curso/resources/CategoryResource.java
@@ -2,6 +2,7 @@ package com.educandoweb.curso.resources;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +14,8 @@ import com.educandoweb.curso.entities.Category;
 import com.educandoweb.curso.services.CategoryService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
-	
+@RequestMapping(value = "/categories")
+public class CategoryResource {
 	
 	@Autowired
 	private CategoryService service;
